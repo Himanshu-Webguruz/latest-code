6c303be63ed9d5e5dec8528d5a0173cbef8d4418
Himanshu-Webguruz <167845496+Himanshu-Webguruz@users.noreply.github.com>
2024-11-26 13:20:10 +0530

    Update page.jsx

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -12,7 +12,7 @@ const page = async ({ searchParams }) => {
   const limit = 10;
 
   const response = await fetch(
-    `${process.env.NEXTAUTH_URL}/api/getdata?page=${page}&limit=${limit}`
+    `${process.env.NEXTAUTH_URL}/api/getdata?page=${page}`
   );
 
   if (!response.ok) {
